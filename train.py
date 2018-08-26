44356a49d9c5073d7dcbcab1916b8c451f7f33bc
Wentao Wang <wwt10@pku.edu.cn>
2018-08-25 22:50:11 -0400

    enhanced save and restore checkpoints

--- train.py
+++ train.py
@@ -19,11 +19,7 @@ from dataloader import *
 import eval_utils
 import misc.utils as utils
 
-try:
-    import tensorflow as tf
-except ImportError:
-    print("Tensorflow not installed; No tensorboard logging.")
-    tf = None
+import tensorflow as tf
 
 def add_summary_value(writer, key, value, iteration):
     summary = tf.Summary(value=[tf.Summary.Value(tag=key, simple_value=value)])
@@ -37,20 +33,28 @@ def train(opt):
 
     tf_summary_writer = tf and tf.summary.FileWriter(opt.checkpoint_path)
 
-    infos = {}
-    histories = {}
-    if opt.start_from is not None:
+    def load_infos(dir=opt.start_from, suffix=''):
         # open old infos and check if models are compatible
-        with open(os.path.join(opt.start_from, 'infos_'+opt.id+'.pkl')) as f:
+        with open(os.path.join(dir, 'infos_{}{}.pkl'.format(opt.id, suffix))) as f:
             infos = cPickle.load(f)
             saved_model_opt = infos['opt']
             need_be_same=["caption_model", "rnn_type", "rnn_size", "num_layers"]
             for checkme in need_be_same:
                 assert vars(saved_model_opt)[checkme] == vars(opt)[checkme], "Command line argument and saved model disagree on '%s' " % checkme
+        return infos
 
-        if os.path.isfile(os.path.join(opt.start_from, 'histories_'+opt.id+'.pkl')):
-            with open(os.path.join(opt.start_from, 'histories_'+opt.id+'.pkl')) as f:
+    def load_histories(dir=opt.start_from, suffix=''):
+        path = os.path.join(dir, 'histories_{}{}.pkl'.format(opt.id, suffix))
+        if os.path.isfile(path):
+            with open(path) as f:
                 histories = cPickle.load(f)
+        return histories
+
+    infos = {}
+    histories = {}
+    if opt.start_from is not None:
+        infos = load_infos()
+        histories = load_histories()
 
     iteration = infos.get('iter', 0)
     epoch = infos.get('epoch', 0)
@@ -79,6 +83,23 @@ def train(opt):
     # Load the optimizer
     if vars(opt).get('start_from', None) is not None:
         optimizer.load_state_dict(torch.load(os.path.join(opt.start_from, 'optimizer.pth')))
+
+    def eval_model():
+        eval_kwargs = {'split': 'val',
+                       'dataset': opt.input_json}
+        eval_kwargs.update(vars(opt))
+        val_loss, predictions, lang_stats = eval_utils.eval_split(model, crit, loader, eval_kwargs)
+
+        # Write validation result into summary
+        if tf is not None:
+            add_summary_value(tf_summary_writer, 'validation loss', val_loss, iteration)
+            for k,v in lang_stats.items():
+                add_summary_value(tf_summary_writer, k, v, iteration)
+            tf_summary_writer.flush()
+
+        return val_loss, predictions, lang_stats
+
+    eval_model()
 
     while True:
         if update_lr_flag:
@@ -140,18 +161,7 @@ def train(opt):
 
         # make evaluation on validation set, and save model
         if (iteration % opt.save_checkpoint_every == 0):
-            # eval model
-            eval_kwargs = {'split': 'val',
-                            'dataset': opt.input_json}
-            eval_kwargs.update(vars(opt))
-            val_loss, predictions, lang_stats = eval_utils.eval_split(model, crit, loader, eval_kwargs)
-
-            # Write validation result into summary
-            if tf is not None:
-                add_summary_value(tf_summary_writer, 'validation loss', val_loss, iteration)
-                for k,v in lang_stats.items():
-                    add_summary_value(tf_summary_writer, k, v, iteration)
-                tf_summary_writer.flush()
+            val_loss, predictions, lang_stats = eval_model()
             val_result_history[iteration] = {'loss': val_loss, 'lang_stats': lang_stats, 'predictions': predictions}
 
             # Save model if is improving on validation result
@@ -160,16 +170,11 @@ def train(opt):
             else:
                 current_score = - val_loss
 
-            best_flag = False
             if True: # if true
+                best_flag = False
                 if best_val_score is None or current_score > best_val_score:
                     best_val_score = current_score
                     best_flag = True
-                checkpoint_path = os.path.join(opt.checkpoint_path, 'model.pth')
-                torch.save(model.state_dict(), checkpoint_path)
-                print("model saved to {}".format(checkpoint_path))
-                optimizer_path = os.path.join(opt.checkpoint_path, 'optimizer.pth')
-                torch.save(optimizer.state_dict(), optimizer_path)
 
                 # Dump miscalleous informations
                 infos['iter'] = iteration
@@ -184,17 +189,24 @@ def train(opt):
                 histories['loss_history'] = loss_history
                 histories['lr_history'] = lr_history
                 histories['ss_prob_history'] = ss_prob_history
-                with open(os.path.join(opt.checkpoint_path, 'infos_'+opt.id+'.pkl'), 'wb') as f:
-                    cPickle.dump(infos, f)
-                with open(os.path.join(opt.checkpoint_path, 'histories_'+opt.id+'.pkl'), 'wb') as f:
-                    cPickle.dump(histories, f)
+
+                def save_model(suffix=''):
+                    model_path = os.path.join(opt.checkpoint_path, 'model{}.pth'.format(suffix))
+                    torch.save(model.state_dict(), model_path)
+                    print("model saved to {}".format(checkpoint_path))
+                    optimizer_path = os.path.join(opt.checkpoint_path, 'optimizer{}.pth'.format(suffix))
+                    torch.save(optimizer.state_dict(), optimizer_path)
+
+                    with open(os.path.join(opt.checkpoint_path, 'infos_{}{}.pkl'.format(opt.id, suffix)), 'wb') as f:
+                        cPickle.dump(infos, f)
+                    with open(os.path.join(opt.checkpoint_path, 'histories_{}{}.pkl'.format(opt.id, suffix)), 'wb') as f:
+                        cPickle.dump(histories, f)
+
+                save_model()
+                save_model(".iter{}".format(iteration))
 
                 if best_flag:
-                    checkpoint_path = os.path.join(opt.checkpoint_path, 'model-best.pth')
-                    torch.save(model.state_dict(), checkpoint_path)
-                    print("model saved to {}".format(checkpoint_path))
-                    with open(os.path.join(opt.checkpoint_path, 'infos_'+opt.id+'-best.pkl'), 'wb') as f:
-                        cPickle.dump(infos, f)
+                    save_model(".best")
 
         # Stop if reaching max epochs
         if epoch >= opt.max_epochs and opt.max_epochs != -1:
